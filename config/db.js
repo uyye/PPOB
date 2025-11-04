fceccdadf6df7807783b00394189b8e958468e33
uyye <sultannursalimm@gmail.com>
2025-11-04 17:15:47 +0800

    deploy

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || 
     "postgres://postgres:admin@localhost:5432/PPOB",
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   idleTimeoutMillis: 500,
 });
 
