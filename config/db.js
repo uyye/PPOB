9ca771e7482517f38b2115956b48101cdc39114c
uyye <sultannursalimm@gmail.com>
2025-11-04 17:39:50 +0800

    -

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
-const { Pool } = require("pg");
+  const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 
-    "postgres://postgres:admin@localhost:5432/PPOB",
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  idleTimeoutMillis: 500,
-});
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || 
+      "postgres://postgres:admin@localhost:5432/PPOB",
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    idleTimeoutMillis: 500,
+  });
 
-module.exports = pool;
+  module.exports = pool;
